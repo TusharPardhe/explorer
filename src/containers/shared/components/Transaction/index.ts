bcaedf11c7016362912ba191966c5c847aee2dfa
Mayukha Vadari <mvadari@ripple.com>
2022-08-18 19:57:08 +0200

    implement XChainAccountCreate

--- src/containers/shared/components/Transaction/index.ts
+++ src/containers/shared/components/Transaction/index.ts
@@ -1,5 +1,6 @@
 import { OfferCreateTransaction as OfferCreate } from './OfferCreate'
 import { SetRegularKeyTransaction as SetRegularKey } from './SetRegularKey'
+import { XChainAccountCreateTransaction as XChainAccountCreate } from './XChainAccountCreate'
 import { XChainClaimTransaction as XChainClaim } from './XChainClaim'
 import { XChainCommitTransaction as XChainCommit } from './XChainCommit'
 import { XChainCreateBridgeTransaction as XChainCreateBridge } from './XChainCreateBridge'
@@ -13,4 +14,5 @@ export const transactionTypes: { [key: string]: TransactionMapping } = {
   XChainCommit,
   XChainCreateBridge,
   XChainCreateClaimID,
+  SidechainXChainAccountCreate: XChainAccountCreate,
 }
